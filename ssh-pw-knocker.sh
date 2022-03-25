da77aea1fa3ff069727fe5ff1b087dd38b589138
dever-ticky <deversoft.andrei.t@gmail.com>
2022-03-25 11:35:59 +0200

    3 step knock function

--- ssh-pw-knocker.sh
+++ ssh-pw-knocker.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
-then
-    echo 'Usage: ./ssh-pw-knocker.sh /path/to/file'
-    echo 'Ex: ./ssh-pw-knocker.sh /etc/hosts'
+# knock and check password authentication is allowed
+knock_ssh_pw () {
+    IP=$1
+    MACHINE=$2
+    # firstly check to see if it's online
+    ping -i 0.2 -c 1 -W 1 $IP > /dev/null 2>&1
+    if [ $? == 0 ] ; then
+        # secondly check to see if ssh is open on standard port
+        nc -z $IP 22
+        if [ $? == 0 ] ; then
+            # if ssh open knock
+            ssh -v -n -o Batchmode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null DOES_NOT_EXIST@$IP 2>&1 | grep password > /dev/null 2>&1
+            if [ $? == 0 ] ; then
+                echo "$IP $MACHINE - !! password authentication allowed !! Fix it !!"
+            else
+                echo "$IP $MACHINE - password authentication disabled"
+            fi
+        else
+            echo "$IP $MACHINE - SSH standard port not open"
+        fi
+    else
+        echo "$IP $MACHINE - offline"
+    fi
+}
+
+# usage
+if [ "$#" -ne 1 ] ; then
+    echo "Usage: ./ssh-pw-knocker.sh /path/to/file"
+    echo "Ex: ./ssh-pw-knocker.sh /etc/hosts"
     exit 1
 fi
+
+# check to see if file is accessible
+if [ ! -r $1 ] ; then
+    echo "File not readable"
+    exit 2
+fi
+
+REGEX_IP_HOST="\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b [^\s]+"
 
 # remove commented lines from file and start reading it
 grep -v '^#' $1 | while IFS= read -r LINE
 do
-    # validate "IP host" pattern
-    REGEX_IP_HOST="\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b [^\s]+"
-
+    # validate "IP host" pattern and knock
     if [[ $LINE =~ $REGEX_IP_HOST ]] ; then
         CURR_IP=$(echo "$LINE" | cut -d " " -f 1)
         CURR_MACHINE=$(echo "$LINE" | cut -d " " -f 2)
-
-        # check to see if it's online firstly
-        nc -z $CURR_MACHINE 22
-        if [ $? == 0 ] ; then
-            IS_ONLINE=true
-        else
-            IS_ONLINE=false
-        fi
-
-        # if online knock
-        if ($IS_ONLINE) ; then
-            ssh -v -n -o Batchmode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null DOES_NOT_EXIST@$CURR_MACHINE 2>&1 | grep password > /dev/null 2>&1
-            if [ $? == 0 ] ; then
-                echo "$CURR_IP $CURR_MACHINE - !! Password Authentication allowed !! Fix it !!"
-            else
-                echo "$CURR_IP $CURR_MACHINE - Password Authentication disabled"
-            fi
-        else
-            echo "$CURR_IP $CURR_MACHINE - not online."
-        fi
+        # knock
+        knock_ssh_pw "$CURR_IP" "$CURR_MACHINE"
+    else
+        echo "Ignore invalid line - $LINE"
     fi
 done
 
